0d9aece3a18108cfcb4e8fa67b19f693c05053ff
alhabsi16 <mxmx8283@gmail.com>
2023-07-11 11:02:18 +0400

    Enum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+
+namespace EnumTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            // DaysOfWeek.DaysOfWeek1();
+
+            //Task 2
+            // Clothing.ClothingSize1();
+
+            //Task 3
+            // Colors.FavoraiteColor1();
+
+            //Task 5
+            TrafficLights.TrafficLightSequence(8000,2000,5000,5);
+
+        }
+    }
+}
